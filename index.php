7dac69683a3eb90f35e58f771fdc12741b4a0351
Andrew R <andrew.manjakasoa@gmail.com>
2017-01-09 12:53:54 +0100

    index modif

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <form method="post" action="Send.php">
-	<input type="text" name="name" placeholder="taper ici votre Nom"/>
+	<input type="text" name="name" placeholder="Nom"/>
 	<input type="text" name="email" placeholder="taper ici votre mail"/>
 	<input type="password" name="pass" placeholder="taper ici votre MDP"/>
 	<input type="submit" value="inscription">
