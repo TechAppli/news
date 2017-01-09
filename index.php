896cc2f4f033f37b56e1a2a2ee393d69fdc2bf98
TechAppli <rakotonirina.benja@hotmail.com>
2017-01-09 10:04:15 +0100

    Update index.php
    modification bouton inscrire

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 	<input type="text" name="name" placeholder="taper ici votre Nom"/>
 	<input type="text" name="email" placeholder="taper ici votre mail"/>
 	<input type="password" name="pass" placeholder="taper ici votre MDP"/>
-	<input type="submit" value="inscrire">
+	<input type="submit" value="inscription">
 </form>
 </html>
